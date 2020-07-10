a4cb72afde0a3cd5e9d9784ce3fda00f11cff0e8
majiasheng <jiasheng.ma@yahoo.com>
2020-07-09 22:32:28 -0400

    add sub path

--- src/pages/art/Art.js
+++ src/pages/art/Art.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import {
-    pedestal,
-    percarious,
-} from './themes'
+    Switch,
+    Route,
+    Link,
+    useRouteMatch
+  } from "react-router-dom";
+import * as themes from './themes'
 
 function Art() {
-  return (
-    <div className="Art">
-        { percarious[0].note }
-    </div>
-  );
+    let { path } = useRouteMatch();
+    let links = [];
+    let routes = [];
+    Object.keys(themes).map( t =>
+        links.push(
+            <li key={`link_${t}`}>
+                <Link to={`${path}/${t}`}>{t}</Link>
+            </li>
+        )
+    )
+    Object.entries(themes).map( ([key,value]) =>
+        routes.push(
+            <Route path={`${path}/${key}`} key={`route_${key}`} >
+                replace with generic art component witht "{key}" content
+            </Route>
+        )
+    )
+
+    return (
+        <div className="Art">
+
+            <Switch>
+                <Route exact path={path}>
+                    <h3>The explicable and inexplicables</h3>
+                    <ul>
+                        {links}
+                    </ul>
+                </Route>
+
+                {routes}
+                
+            </Switch>
+        </div>
+    );
 }
 
 export default Art;
