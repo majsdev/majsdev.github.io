c1a8467cd73a8a0f8b212308860772c91dc86f15
majiasheng <jiasheng.ma@yahoo.com>
2020-07-18 23:09:15 -0400

    show project desc

--- src/pages/work/WorkDisplay.js
+++ src/pages/work/WorkDisplay.js
@@ -11,11 +11,21 @@ import React from "react";
  * }
  */
 function WorkDisplay({ work }) {
+  const { id, name, desc, techStack } = work;
+  let techStackJSX = [];
+  for (const [index, tech] of Object.entries(techStack) ) {
+    techStackJSX.push(<span key={`tech-stack-${id}-${index}`}>{tech}</span>);
+  }
+
   return (
     <div className="WorkDisplay">
-      <h4>{work.name}</h4>
+      <h4>{name}</h4>
       <div className="work-display-body">
         {/* <img src={require(`${work.thumbNail}`)} alt={work.name}></img> */}
+        <p class="desc" v-html="desc">{desc}</p>
+        <div class="tech-stack">
+          {techStackJSX}
+        </div>
       </div>
     </div>
   );
