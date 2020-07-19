4a645bff1869b69f1b1c35a60abe967291855315
majiasheng <jiasheng.ma@yahoo.com>
2020-07-18 21:03:09 -0400

    add art theme; allow arrow control in work page

--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import projects from './projects';
 import {
     Switch,
@@ -6,26 +6,41 @@ import {
     Link,
     useRouteMatch
 } from "react-router-dom";
+import {
+    SELECTED_ITEM
+} from '../includes/cssClasses'; 
 import WorkDisplay from './WorkDisplay';
 
-function Work() {
+function Work(props) {
     let { path } = useRouteMatch();
     let links = [];
     let routes = [];
 
+    useEffect(() => {
+        props.setNumOfSelectableItems(Object.entries(projects).length);
+        return () => {
+            props.setNumOfSelectableItems(0);
+        }
+    });
+
+    let index = 0;
     Object.entries(projects).map( ([key, value]) => {
             links.push(
-                <li key={`link_${key}`}>
+                <li
+                    key={`link_${key}`}
+                    className={`${props.indexOfSelectableItem === index ? SELECTED_ITEM : ''}`}
+                >
                     <Link to={`${path}/${value.id}`}>• {value.name}</Link>
                 </li>
-            )
+            );
             routes.push(
                 <Route path={`${path}/${value.id}`} key={`route_${key}`} >
                     <WorkDisplay
                         work={value}
                     />
                 </Route>
-            )
+            );
+            index++;
         }
     )
     routes.push(
@@ -39,7 +54,7 @@ function Work() {
             <Switch>
                 <Route exact path={path}>
                     <h3>Projects and work items</h3>
-                    <ul>
+                    <ul className="inline-block">
                         {links}
                     </ul>
                 </Route>
