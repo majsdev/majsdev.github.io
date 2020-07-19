9db5f35be928ffa0659e56e891a547424b0a48b9
majiasheng <jiasheng.ma@yahoo.com>
2020-07-18 23:00:52 -0400

    update formats

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import {
-  About,
-  Art,
-  Home,
-  Note,
-  Work,
-  NavBar,
-  Console
-} from './pages';
-import './App.css';
-import { NUMBER_OF_ROUTES } from './routes';
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { About, Art, Home, Note, Work, NavBar, Console } from "./pages";
+import "./App.css";
+import { NUMBER_OF_ROUTES } from "./routes";
 
 function App() {
   const [isMenuOpened, setIsMenuOpened] = useState(false);
   const [indexOfSelectedRoute, setIndexOfSelectedRoute] = useState(0);
-  const [numOfSelectableItems, setNumOfSelectableItems] = useState(0);  /* doubles as indicator of arrow selectable items on the screen */
+  const [numOfSelectableItems, setNumOfSelectableItems] = useState(
+    0
+  ); /* doubles as indicator of arrow selectable items on the screen */
   const [indexOfSelectableItem, setIndexOfSelectableItem] = useState(0);
   const closeMenu = () => setIsMenuOpened(false);
   const openMenu = () => setIsMenuOpened(true);
@@ -28,67 +17,65 @@ function App() {
     if (!isMenuOpened) {
       openMenu();
     } else {
-      setIndexOfSelectedRoute((indexOfSelectedRoute+1)%NUMBER_OF_ROUTES);
+      setIndexOfSelectedRoute((indexOfSelectedRoute + 1) % NUMBER_OF_ROUTES);
     }
-  }
+  };
   const handleBOnClick = () => {
     if (isMenuOpened) {
       closeMenu();
       setIndexOfSelectedRoute(0);
     }
-  }
+  };
   const handleMouseSelectNavItem = (index) => () => {
-    if(isMenuOpened)
-      setIndexOfSelectedRoute(index);
-  }
+    if (isMenuOpened) setIndexOfSelectedRoute(index);
+  };
   const handleUpArrowOnClick = () => {
-    if(!isMenuOpened && numOfSelectableItems > 0) {
+    if (!isMenuOpened && numOfSelectableItems > 0) {
       /* + numOfSelectableItems because negative number mod positive number will return negative */
-      setIndexOfSelectableItem((indexOfSelectableItem-1+numOfSelectableItems)%numOfSelectableItems);
+      setIndexOfSelectableItem(
+        (indexOfSelectableItem - 1 + numOfSelectableItems) %
+          numOfSelectableItems
+      );
     }
-  }
+  };
   const handleDownArrowOnClick = () => {
-    if(!isMenuOpened && numOfSelectableItems > 0)
-      setIndexOfSelectableItem((indexOfSelectableItem+1)%numOfSelectableItems);
-  }
+    if (!isMenuOpened && numOfSelectableItems > 0)
+      setIndexOfSelectableItem(
+        (indexOfSelectableItem + 1) % numOfSelectableItems
+      );
+  };
 
   return (
     <div className="App">
       <Router>
         <div className="container">
           <div className="content">
-                <Switch>
-                  <Route exact path="/">
-                    <Home
-                      setNumOfSelectableItems={setNumOfSelectableItems}
-                    />
-                  </Route>
-                  <Route path="/about">
-                    <About
-                      setNumOfSelectableItems={setNumOfSelectableItems}
-                    />
-                  </Route>
-                  <Route path="/art">
-                    <Art
-                      setNumOfSelectableItems={setNumOfSelectableItems}
-                      setIndexOfSelectableItem={setIndexOfSelectableItem}
-                      indexOfSelectableItem={indexOfSelectableItem}
-                    />
-                  </Route>
-                  <Route path="/note">
-                    <Note
-                      setNumOfSelectableItems={setNumOfSelectableItems}
-                    />
-                  </Route>
-                  <Route path="/work">
-                    <Work
-                      setNumOfSelectableItems={setNumOfSelectableItems}
-                      setIndexOfSelectableItem={setIndexOfSelectableItem}
-                      indexOfSelectableItem={indexOfSelectableItem}
-                    />
-                  </Route>
-                  <Route path="*"> 404 </Route>
-                </Switch>
+            <Switch>
+              <Route exact path="/">
+                <Home setNumOfSelectableItems={setNumOfSelectableItems} />
+              </Route>
+              <Route path="/about">
+                <About setNumOfSelectableItems={setNumOfSelectableItems} />
+              </Route>
+              <Route path="/art">
+                <Art
+                  setNumOfSelectableItems={setNumOfSelectableItems}
+                  setIndexOfSelectableItem={setIndexOfSelectableItem}
+                  indexOfSelectableItem={indexOfSelectableItem}
+                />
+              </Route>
+              <Route path="/note">
+                <Note setNumOfSelectableItems={setNumOfSelectableItems} />
+              </Route>
+              <Route path="/work">
+                <Work
+                  setNumOfSelectableItems={setNumOfSelectableItems}
+                  setIndexOfSelectableItem={setIndexOfSelectableItem}
+                  indexOfSelectableItem={indexOfSelectableItem}
+                />
+              </Route>
+              <Route path="*"> 404 </Route>
+            </Switch>
           </div>
         </div>
 
--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -1,74 +1,62 @@
-import React from 'react';
-import projects from './projects';
-import {
-    Switch,
-    Route,
-    Link,
-    withRouter
-} from "react-router-dom";
-import {
-    SELECTED_ITEM
-} from '../includes/cssClasses'; 
-import WorkDisplay from './WorkDisplay';
+import React from "react";
+import projects from "./projects";
+import { Switch, Route, Link, withRouter } from "react-router-dom";
+import { SELECTED_ITEM } from "../includes/cssClasses";
+import WorkDisplay from "./WorkDisplay";
 
 class Work extends React.Component {
+  componentDidMount() {
+    this.props.setNumOfSelectableItems(Object.entries(projects).length);
+  }
 
-    componentDidMount() {
-        this.props.setNumOfSelectableItems(Object.entries(projects).length);
-    }
+  componentWillUnmount() {
+    this.props.setNumOfSelectableItems(0);
+    this.props.setIndexOfSelectableItem(0);
+  }
 
-    componentWillUnmount() {
-        this.props.setNumOfSelectableItems(0);
-        this.props.setIndexOfSelectableItem(0);
-    }
+  render() {
+    let { path } = this.props.match; /* `match` is from `withRouter` */
+    let links = [];
+    let routes = [];
 
-    render() {
-        let { path } = this.props.match;  /* `match` is from `withRouter` */
-        let links = [];
-        let routes = [];
+    let index = 0;
+    Object.entries(projects).map(([key, value]) => {
+      links.push(
+        <li
+          key={`link_${key}`}
+          className={`${
+            this.props.indexOfSelectableItem === index ? SELECTED_ITEM : ""
+          }`}
+        >
+          <Link to={`${path}/${value.id}`}>• {value.name}</Link>
+        </li>
+      );
+      routes.push(
+        <Route path={`${path}/${value.id}`} key={`route_${key}`}>
+          <WorkDisplay work={value} />
+        </Route>
+      );
+      index++;
+    });
+    routes.push(
+      <Route path={`${path}/*`} key={`route_404`}>
+        404
+      </Route>
+    );
 
-        let index = 0;
-        Object.entries(projects).map( ([key, value]) => {
-                links.push(
-                    <li
-                        key={`link_${key}`}
-                        className={`${this.props.indexOfSelectableItem === index ? SELECTED_ITEM : ''}`}
-                    >
-                        <Link to={`${path}/${value.id}`}>• {value.name}</Link>
-                    </li>
-                );
-                routes.push(
-                    <Route path={`${path}/${value.id}`} key={`route_${key}`} >
-                        <WorkDisplay
-                            work={value}
-                        />
-                    </Route>
-                );
-                index++;
-            }
-        )
-        routes.push(
-            <Route path={`${path}/*`} key={`route_404`} >
-                404
-            </Route>
-        )
+    return (
+      <div className="Work">
+        <Switch>
+          <Route exact path={path}>
+            <h3>Projects and work items</h3>
+            <ul className="inline-block">{links}</ul>
+          </Route>
 
-        return (
-            <div className="Work">
-                <Switch>
-                    <Route exact path={path}>
-                        <h3>Projects and work items</h3>
-                        <ul className="inline-block">
-                            {links}
-                        </ul>
-                    </Route>
-
-                    {routes}
-                    
-                </Switch>
-            </div>
-        );
-    }
+          {routes}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Work);
