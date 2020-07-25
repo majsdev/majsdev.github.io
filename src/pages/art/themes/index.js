439828e86cdd343fa6d3b2d0e1e40161d7b5303d
majiasheng <jiasheng.ma@yahoo.com>
2020-07-24 23:01:13 -0400

    add art theme

--- src/pages/art/themes/index.js
+++ src/pages/art/themes/index.js
@@ -1,3 +1,4 @@
 export { default as precarious } from './precarious';
 export { default as pedestal } from './pedestal';
 export { default as glamorous } from './glamorous';
+export { default as minimal } from './minimal'
---
+++ src/pages/art/themes/minimal.js
@@ -0,0 +1,9 @@
+const minimal = [
+    {
+      imageLocation: '',
+      note: 'minimal #1',
+      date: '2017-12-01'
+    }
+  ];
+  
+  export default minimal;
