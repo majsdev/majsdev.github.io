1fff6f0ac826c4da8f0d1bfaeee96affa6a33c2c
mjs <majs.dev@gmail.com>
2021-11-20 23:16:03 -0500

    (WIP) add post

--- src/pages/posts/_posts/post11182021.js
+++ src/pages/posts/_posts/post11182021.js
@@ -1,7 +1,13 @@
 const post = {
-    title: 'Beginning of posts',
+    title: 'Beginning of Posts',
     date: '11/18/2021',
-    content: `A friend who is just getting started on learning programming reached out to me and asked`
+    content: `A friend who is just getting started on learning programming reached out to me and asked "... is learning the meaning/usage/definition of a command line the most important about coding? ... I can probably search all the [command lines] of a specific program on google; and syntax is too annoying. ... I want to understand how to 'learn' first."
+    I thought he was referring to command line (cli), so I gave him some background of command line and the difference between cli and GUI programs.
+    However, I was quick to assume because he later aligned me on what he really was trying to understand
+    that got me to think back to how I started - i was too, unsure of where to begin and what questions to ask. So direction is very important in learning, not just programming but all learnings as well.
+    helping people getting started in learning how to code is part of the main reason why i started this *posts* section
+    `
+
 }
 
 export default post;
