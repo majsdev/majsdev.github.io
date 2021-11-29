6ff149d45840553c40d1743888a5f0078e316ca4
mjs <majs.dev@gmail.com>
2021-11-28 22:54:05 -0500

    (WIP) add post

--- src/pages/posts/_posts/index.js
+++ src/pages/posts/_posts/index.js
@@ -1,2 +1,3 @@
 export { default as post11182021 } from "./post11182021";
 export { default as post11202021 } from "./post11202021";
+export { default as post11272021 } from "./post11272021";
---
+++ src/pages/posts/_posts/post11272021.js
@@ -0,0 +1,48 @@
+const post = {
+    title: 'Overview Of Building an Application',
+    date: '11/27/2021',
+    content: `
+    <div>
+    <h2>0. Foreword</h2>
+    This post is intended to serve as an overview for new comers in app development.
+    When I first started, I have little idea in what are all the relevant pieces in app development, and there is a myriad of terms that I wish I knew and their connections to the context.
+    I will try to include as many relevant terms as possible. It's okay if you don't understand all of them,
+    because it's my intention to expose you to new terms so you will have an easier time connecting the dots when you come across them down your learning path.
+    </div>
+    <hr>
+    <div>
+    There are a few main steps in building an application end to end, they are:
+    1. Design
+    2. Develop
+    3. Test (repeat 1 and 2 when needed)
+    4. Deploy
+    </div>
+
+    <div>
+    <h2>1. Design</h2>
+    First thing first, to start any project, there's going to be a design phase. 
+    In this very first step, we are putting ideas in concrete terms and actionable items.
+    For example, say we want to make a vending machine. 
+    First, we will need to define the scope of work; namely, what do we want the vending machine to do (what are the *features*)?
+    With this question in mind, we are coming up with use cases, which will provide direction for us to make technical decisions such as programming lanauge, algorithm, architecture, and etc.
+
+    </div>
+
+    <div>
+    <h2>2. Develop</h2>
+    </div>
+    This is part where coding starts. Some people think this is what a programmer/coder/software engineer does mostly, but this is just part of the whole app development.
+    You may not need a 100% well thought out design, but having a design of the 
+
+    <div>
+    <h2>3. Test</h2>
+    </div>
+
+    <div>
+    <h2>4. Deploy</h2>
+    </div>
+    `
+
+}
+
+export default post;
