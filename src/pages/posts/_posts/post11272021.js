082b757e77b575d4819e6f5da7ebea12c95bf915
mjs <majs.dev@gmail.com>
2021-11-30 23:07:13 -0500

    (WIP) write post

--- src/pages/posts/_posts/post11272021.js
+++ src/pages/posts/_posts/post11272021.js
@@ -14,32 +14,40 @@ const post = {
     There are a few main steps in building an application end to end, they are:
     1. Design
     2. Develop
-    3. Test (repeat 1 and 2 when needed)
-    4. Deploy
+    3. Deploy
     </div>
 
     <div>
     <h2>1. Design</h2>
     First thing first, to start any project, there's going to be a design phase. 
     In this very first step, we are putting ideas in concrete terms and actionable items.
-    For example, say we want to make a vending machine. 
-    First, we will need to define the scope of work; namely, what do we want the vending machine to do (what are the *features*)?
-    With this question in mind, we are coming up with use cases, which will provide direction for us to make technical decisions such as programming lanauge, algorithm, architecture, and etc.
-
+    We will need to define the scope of work; namely, what do we want the vending machine to do (what are the *features*)?
+    With this question in mind, we are coming up with use cases, for which we will make technical decisions such as programming lanauge, algorithm, architecture, and etc.
     </div>
 
     <div>
     <h2>2. Develop</h2>
+    This is where the actual programming starts.
+    By programming I meaning implementing application logic, and testing the application, 
+    by means of writing and executing tests or testing the application interactively via CLI, UI, or other means depending on the nature of the application.
+    As the project goes on, you will find yourself repeating the steps. 
+    And a lot of the times, you may find yourself spending more time debugging the code you wrote than the time you spent in writing the code.
+
+    If you are working in a formal environment, you may also need to go through code reviews after you submit a what's called a pull request (sometimes it is call a merge request).
+    What exactly goes in the code review depends on your group.
+    But as the name suggests, your code will be reviewed by other people, and you may or may not get comments, or approval, depending on your code.
+
+    This develop, test, debug, and code review cycle is the bulk of the *development* part of building an application.
+    As I hinted, it's a cycle; therefore, you will need to iterate through it enough times until it is ready to be deployed.
+    Sometimes, you may need to go back to the design phase and revise the plan.
+
+    Adding to the code writing portion of this section, *version control* is something that you will be working with inseparably. 
+    What this is 
     </div>
-    This is part where coding starts. Some people think this is what a programmer/coder/software engineer does mostly, but this is just part of the whole app development.
-    You may not need a 100% well thought out design, but having a design of the 
 
     <div>
-    <h2>3. Test</h2>
-    </div>
-
-    <div>
-    <h2>4. Deploy</h2>
+    <h2>3. Deploy</h2>
+    When your application matures to the point where it is ready to go live for users to use, you are going to deploy it.
     </div>
     `
 
